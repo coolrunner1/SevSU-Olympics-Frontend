efa22cd33b5ac1fa76a83a679a2cd74039bf7680
coolrunner1 <153802775+coolrunner1@users.noreply.github.com>
2025-09-05 09:25:23 +0300

    Vercel navigation fix

--- src/validationSchemas/auth.ts
+++ src/validationSchemas/auth.ts
@@ -1,6 +1,6 @@
 import {z} from "zod";
 
 export const LoginSchema = z.object({
-    login: z.string().min(3, "Логин должен содержать от 6 символов"),
+    login: z.string().min(6, "Логин должен содержать от 6 символов"),
     password: z.string().min(8,  "Пароль должен содержать от 8 символов"),
 });
